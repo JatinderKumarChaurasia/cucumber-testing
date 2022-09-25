3f71a2be435664ce354e8cdb5b86059414d2d8e6
Jatinder Kumar Chaurasia <jatinderkumarchaurasia@gmail.com>
2022-09-25 22:20:01 +0530

    test1

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -1,1 +1,23 @@
 echo 'hello jenkins'
+
+void clone_repo() {
+    println("cloning repo")
+    checkout([$class           : 'GitSCM',
+              branches         : [[name: '*/master']],
+              userRemoteConfigs: [[url: 'https://github.com/jenkinsci/git-plugin.git']]])
+}
+
+pipeline{
+    agent any
+    tools {git "git"}
+
+    stages {
+        stage ('build'){
+            steps{
+                sh 'gradle --version'
+                sh 'git --version'
+                clone_repo()
+            }
+        }
+    }
+}
